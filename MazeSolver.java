e1b00f31f3115cd6d38453617c5913e3551c9b16
CarlosCapili <capilc1@mcmaster.ca>
2021-06-18 16:10:37 -0400

    Update

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,57 +1,66 @@
 /*
-This program finds a path to the bottom right corner of a maze using recursion, 
+This program finds a path to the bottom right corner of a maze using recursion starting from the first index, 
 where 0's represent walls, 1 represents a possible path, and 8 is the path taken
 */
-
 class MazeSolver{
 	public static void main(String[]arg){
 		Maze path = new Maze();
 		path.displayMaze();
 
-		if(path.recursiveSolver(0,0) == 0){
+		if(path.recursiveSolver(0,0) == false){
 			System.out.println("You failed to find a way out!");
 			path.displayMaze();
 		}
 		else{
-			path.displayMaze();
 			System.out.println("You made it through!");
+			path.displayMaze();	
 		}
 	}
 }
 
 class Maze{
-
 	private int[][] maze = {
-		{1,1,0,1,1},
-		{0,1,0,0,0},
-		{0,1,1,1,0},
-		{0,0,0,1,1}
+		{1,1,1,1,1},
+		{0,1,0,0,1},
+		{0,1,0,0,1},
+		{1,1,0,1,0},
+		{0,1,1,1,1}
 	};
 
-	//Only goes right and down for now
-	public int recursiveSolver(int row, int col){
-
-		if(maze[row][col] == 1){
+	public boolean recursiveSolver(int row, int col){
+		
+		if (checkRowBounds(row) && checkColBounds(col) && maze[row][col] == 1 && maze[row][col] == 1){
 			//Change the maze path to an 8 indicating path taken
 			maze[row][col] = 8;
-
 			//Check if last element in 2d array is reached
-			if(row == maze.length-1 && col == maze[0].length-1){
-				return 1;
+			if (row == maze.length-1 && col == maze[0].length-1){
+				return true;
 			}
-
-			//Go right
-			if(maze[row][col+1] == 1){
-				return recursiveSolver(row, col+1);
+			//Path movements
+			if (recursiveSolver(row, col+1)){
+				return true; //Move right
 			}
-			else{
-				return recursiveSolver(row+1, col);
+			if (recursiveSolver(row+1, col)){
+				return true; //Move down
 			}
+			if (recursiveSolver(row, col-1)){
+				return true;//Move left
+			}
+			if (recursiveSolver(row-1, col)){
+				return true;//Move Up
+			}
+			maze[row][col] = 6; 
+			return false; //If it makes it here then it is false
 		}
-		else{
-			return 0; //base case
-		}
+		return false;
 	}// end of recursiveSolver
+
+	private boolean checkColBounds(int col){
+		return (col >= 0) && (col < maze[0].length);
+	}
+	private boolean checkRowBounds(int row){
+		return (row >= 0) && (row < maze.length);
+	}
 
 	public void displayMaze(){
 		for(int i=0; i<maze.length; i++){
@@ -63,4 +72,4 @@ class Maze{
 		}
 	}
 		
-}
+}//end of class Maze
