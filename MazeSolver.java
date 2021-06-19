5bf8daefc4c4718e1697754234c1c302b5230833
CarlosCapili <capilc1@mcmaster.ca>
2021-06-19 00:50:18 -0400

    Add importing file feature

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,11 +1,27 @@
 /*
-This program finds a path to the bottom right corner of a maze using recursion starting from the first index, 
-where 0's represent walls, 1 represents a possible path, and 8 is the path taken
+This program finds a possible pathway from the top left corner 
+to the bottom right corner using recursion.
+
+0s are walls and 1s are possible pathways
 */
+import java.util.Scanner;
+
 class MazeSolver{
 	public static void main(String[]arg){
+		
 		Maze path = new Maze();
-		path.displayMaze();
+		int menu = 0;
+
+		//MENU
+		Scanner in = new Scanner(System.in);
+		System.out.println("Welcome to Maze Solver!");
+		System.out.println("\n1. When importing a textfile, make sure the rows and column lengths are equal");
+		System.out.println("2. Make sure the all the values are either 1s or 0s, where 1s are walkable paths and 0s are walls");
+		System.out.println("3. Values must be separated by commas. Ex. 0,1,0,0\n");
+		System.out.println("Please enter the name of the file with the maze you want to be solved:");
+		String fileName = in.nextLine();
+		//pass filename to generateMaze function
+		path.generateMaze(fileName);
 
 		if(path.recursiveSolver(0,0) == false){
 			System.out.println("You failed to find a way out!");
@@ -19,13 +35,14 @@ class MazeSolver{
 }
 
 class Maze{
-	private int[][] maze = {
-		{1,1,1,1,1},
-		{0,1,0,0,1},
-		{0,1,0,0,1},
-		{1,1,0,1,0},
-		{0,1,1,1,1}
-	};
+	private int[][] maze;
+
+	//make get/set methods for private maze
+
+	public void generateMaze(String fileName){
+		//open file and read each line as a row and place into maze
+
+	}
 
 	public boolean recursiveSolver(int row, int col){
 		
